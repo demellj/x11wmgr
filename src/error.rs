ecf5243e95bf217c255e49b27ad8d252ab8694aa
John Mark de Mello <demellj@users.noreply.github.com>
2025-05-07 22:08:46 -0400

    Use thiserror instead of failure crate

--- src/error.rs
+++ src/error.rs
@@ -1,56 +1,56 @@
-use failure::{Backtrace, Context, Fail};
-use x11rb::rust_connection::ReplyOrIdError;
 use std::fmt::{Display, Formatter, Result as DisplayResult};
 use std::sync::Arc;
+use thiserror::Error;
+use x11rb::rust_connection::ReplyOrIdError;
 
 use std::io::Error as IOError;
-use std::sync::mpsc::{SendError, RecvError};
-use x11rb::errors::ConnectionError;
-use x11rb::errors::ReplyError;
-use x11rb::errors::ParseError;
+use std::sync::mpsc::{RecvError, SendError};
 use x11rb::errors::ConnectError;
+use x11rb::errors::ConnectionError;
+use x11rb::errors::ParseError;
+use x11rb::errors::ReplyError;
 use x11rb::protocol::xproto::ACCESS_ERROR;
 use x11rb::x11_utils::X11Error;
 
-use crate::Request;
 use crate::cli::Response;
+use crate::Request;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum ErrorKind {
-    #[fail(display = "Failed to connect to server")]
-    ConnectError(#[cause] ConnectError),
+    #[error("Failed to connect to server")]
+    ConnectError(#[from] ConnectError),
 
-    #[fail(display = "Connection terminated")]
-    ConnectionError(#[cause] ConnectionError),
+    #[error("Connection terminated")]
+    ConnectionError(#[from] ConnectionError),
 
-    #[fail(display = "Resource exhausted: {}", _0)]
+    #[error("Resource exhausted: {0}")]
     ResourceExhausted(&'static str),
 
-    #[fail(display = "An X11 error occurred")]
+    #[error("An X11 error occurred")]
     X11Error(X11Error),
 
-    #[fail(display = "An IO error occurred")]
-    IOError(#[cause] IOError),
+    #[error("An IO error occurred")]
+    IOError(#[from] IOError),
 
-    #[fail(display = "An internal error occurred")]
-    ParseError(#[cause] ParseError),
+    #[error("An internal error occurred")]
+    ParseError(#[from] ParseError),
 
-    #[fail(display = "An internal error occurred")]
-    SendResponseError(#[cause] SendError<Response>),
+    #[error("An internal error occurred")]
+    SendResponseError(#[from] SendError<Response>),
 
-    #[fail(display = "An internal error occurred")]
-    SendRequestError(#[cause] SendError<Request>),
+    #[error("An internal error occurred")]
+    SendRequestError(#[from] SendError<Request>),
 
-    #[fail(display = "An internal error occurred")]
-    RecvError(#[cause] RecvError),
+    #[error("An internal error occurred")]
+    RecvError(#[from] RecvError),
 }
 
 #[derive(Debug, Clone)]
-pub struct Error(Arc<Context<ErrorKind>>);
+pub struct Error(Arc<ErrorKind>);
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> DisplayResult {
-        let ctx = self.0.get_context();
+        let ctx = &self.0.as_ref();
         match ctx {
             ErrorKind::X11Error(err) => {
                 if err.error_code == ACCESS_ERROR {
@@ -64,31 +64,19 @@ impl Display for Error {
     }
 }
 
-impl Fail for Error {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.0.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.0.backtrace()
-    }
-}
+impl std::error::Error for Error {}
 
 impl From<IOError> for Error {
     fn from(error: IOError) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::IOError(error))))
+        Error(Arc::new(ErrorKind::IOError(error)))
     }
 }
 
 impl From<ReplyError> for Error {
     fn from(error: ReplyError) -> Self {
         match error {
-            ReplyError::X11Error(err) => {
-                Error(Arc::new(Context::new(ErrorKind::X11Error(err))))
-            }
-            ReplyError::ConnectionError(err) => {
-                Error(Arc::new(Context::new(ErrorKind::ConnectionError(err))))
-            }
+            ReplyError::X11Error(err) => Error(Arc::new(ErrorKind::X11Error(err))),
+            ReplyError::ConnectionError(err) => Error(Arc::new(ErrorKind::ConnectionError(err))),
         }
     }
 }
@@ -96,14 +84,10 @@ impl From<ReplyError> for Error {
 impl From<ReplyOrIdError> for Error {
     fn from(error: ReplyOrIdError) -> Self {
         match error {
-            ReplyOrIdError::IdsExhausted => {
-                Error(Arc::new(Context::new(ErrorKind::ResourceExhausted("XID"))))
-            }
-            ReplyOrIdError::X11Error(err) => {
-                Error(Arc::new(Context::new(ErrorKind::X11Error(err))))
-            }
+            ReplyOrIdError::IdsExhausted => Error(Arc::new(ErrorKind::ResourceExhausted("XID"))),
+            ReplyOrIdError::X11Error(err) => Error(Arc::new(ErrorKind::X11Error(err))),
             ReplyOrIdError::ConnectionError(err) => {
-                Error(Arc::new(Context::new(ErrorKind::ConnectionError(err))))
+                Error(Arc::new(ErrorKind::ConnectionError(err)))
             }
         }
     }
@@ -111,42 +95,42 @@ impl From<ReplyOrIdError> for Error {
 
 impl From<ConnectError> for Error {
     fn from(error: ConnectError) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::ConnectError(error))))
+        Error(Arc::new(ErrorKind::ConnectError(error)))
     }
 }
 
 impl From<ParseError> for Error {
     fn from(error: ParseError) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::ParseError(error))))
+        Error(Arc::new(ErrorKind::ParseError(error)))
     }
 }
 
 impl From<X11Error> for Error {
     fn from(error: X11Error) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::X11Error(error))))
+        Error(Arc::new(ErrorKind::X11Error(error)))
     }
 }
 
 impl From<SendError<Response>> for Error {
     fn from(error: SendError<Response>) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::SendResponseError(error))))
+        Error(Arc::new(ErrorKind::SendResponseError(error)))
     }
 }
 
 impl From<SendError<Request>> for Error {
     fn from(error: SendError<Request>) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::SendRequestError(error))))
+        Error(Arc::new(ErrorKind::SendRequestError(error)))
     }
 }
 
 impl From<RecvError> for Error {
     fn from(error: RecvError) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::RecvError(error))))
+        Error(Arc::new(ErrorKind::RecvError(error)))
     }
 }
 
 impl From<ConnectionError> for Error {
     fn from(error: ConnectionError) -> Self {
-        Error(Arc::new(Context::new(ErrorKind::ConnectionError(error))))
+        Error(Arc::new(ErrorKind::ConnectionError(error)))
     }
 }

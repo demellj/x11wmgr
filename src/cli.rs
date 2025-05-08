26b800eabc98e9b0c029060cc3752a546e3dfd01
John Mark de Mello <demellj@users.noreply.github.com>
2025-05-07 23:16:52 -0400

    Add unit tests for serialziation

--- src/cli.rs
+++ src/cli.rs
@@ -8,33 +8,33 @@ use serde_json::{de, ser};
 use crate::error::*;
 use crate::windowmanager::{Waker, Window, ZIndexType};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct WinResize {
     pub id: Window,
     pub width: u32,
     pub height: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct WinMove {
     pub id: Window,
     pub x: i32,
     pub y: i32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct WinVisbilty {
     pub id: Window,
     pub visible: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct WinZIndex {
     pub id: Window,
     pub zindex: ZIndexType,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Request {
     ChangeVisibility(Vec<WinVisbilty>),
     ChangeZIndex(Vec<WinZIndex>),
@@ -48,7 +48,7 @@ pub enum Request {
     Commit,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct NewWindow {
     pub id: Window,
     pub x: i32,
@@ -57,7 +57,7 @@ pub struct NewWindow {
     pub height: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Response {
     VisibiltyChanged(Vec<Window>),
     ZIndexChanged(Vec<Window>),
---
+++ tests/serde_tests.rs
@@ -0,0 +1,189 @@
+use serde_json;
+use x11wmgr::{NewWindow, Request, Response, WinMove, WinResize, WinVisbilty, WinZIndex};
+
+#[test]
+fn test_request_move_windows_serialization() {
+    let request = Request::MoveWindows(vec![
+        WinMove {
+            id: 1,
+            x: 100,
+            y: 200,
+        },
+        WinMove {
+            id: 2,
+            x: -50,
+            y: -75,
+        },
+    ]);
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#"{"MoveWindows":[{"id":1,"x":100,"y":200},{"id":2,"x":-50,"y":-75}]}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_request_resize_windows_serialization() {
+    let request = Request::ResizeWindows(vec![
+        WinResize {
+            id: 1,
+            width: 800,
+            height: 600,
+        },
+        WinResize {
+            id: 2,
+            width: 1024,
+            height: 768,
+        },
+    ]);
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#"{"ResizeWindows":[{"id":1,"width":800,"height":600},{"id":2,"width":1024,"height":768}]}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_request_change_visibility_serialization() {
+    let request = Request::ChangeVisibility(vec![
+        WinVisbilty {
+            id: 1,
+            visible: true,
+        },
+        WinVisbilty {
+            id: 2,
+            visible: false,
+        },
+    ]);
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#"{"ChangeVisibility":[{"id":1,"visible":true},{"id":2,"visible":false}]}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_request_change_zindex_serialization() {
+    let request = Request::ChangeZIndex(vec![
+        WinZIndex {
+            id: 1,
+            zindex: 10,
+        },
+        WinZIndex {
+            id: 2,
+            zindex: 20,
+        },
+    ]);
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#"{"ChangeZIndex":[{"id":1,"zindex":10},{"id":2,"zindex":20}]}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_request_list_new_windows_serialization() {
+    let request = Request::ListNewWindows;
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#""ListNewWindows""#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_request_commit_serialization() {
+    let request = Request::Commit;
+
+    let serialized = serde_json::to_string(&request).unwrap();
+    let expected = r#""Commit""#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Request = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, request);
+}
+
+#[test]
+fn test_response_new_windows_serialization() {
+    let response = Response::NewWindows(vec![
+        NewWindow {
+            id: 1,
+            x: 100,
+            y: 200,
+            width: 800,
+            height: 600,
+        },
+        NewWindow {
+            id: 2,
+            x: -50,
+            y: -75,
+            width: 1024,
+            height: 768,
+        },
+    ]);
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    let expected = r#"{"NewWindows":[{"id":1,"x":100,"y":200,"width":800,"height":600},{"id":2,"x":-50,"y":-75,"width":1024,"height":768}]}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Response = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, response);
+}
+
+#[test]
+fn test_response_commit_complete_serialization() {
+    let response = Response::CommitComplete;
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    let expected = r#""CommitComplete""#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Response = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, response);
+}
+
+#[test]
+fn test_response_move_complete_serialization() {
+    let response = Response::MoveComplete;
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    let expected = r#""MoveComplete""#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Response = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, response);
+}
+
+#[test]
+fn test_response_resize_complete_serialization() {
+    let response = Response::ResizeComplete;
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    let expected = r#""ResizeComplete""#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Response = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, response);
+}
+
+#[test]
+fn test_response_window_focused_serialization() {
+    let response = Response::WindowFocused(true);
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    let expected = r#"{"WindowFocused":true}"#;
+    assert_eq!(serialized, expected);
+
+    let deserialized: Response = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, response);
+}
